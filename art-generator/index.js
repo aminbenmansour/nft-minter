e6233405bb25a3408989945876add6dd99b5ff9b
aminbenmansour <amin.benmansour.10@gmail.com>
2022-04-21 15:56:02 +0100

    generate random names for NFTs

--- art-generator/index.js
+++ art-generator/index.js
@@ -12,6 +12,9 @@ const template = `
         <!-- beard -->
     </svg>
 `
+
+const takenNames = {};
+
 // get a random number
 function randInt(max) {
     return Math.floor(Math.random() * (max + 1));
@@ -20,4 +23,23 @@ function randInt(max) {
 // get a random elemnt from array
 function randElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
+}
+
+
+
+function getRandomName() {
+    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
+    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
+    
+    const randAdj = randElement(adjectives);
+    const randName = randElement(names);
+    const name =  `${randAdj}-${randName}`;
+
+
+    if (takenNames[name] || !name) {
+        return getRandomName();
+    } else {
+        takenNames[name] = name;
+        return name;
+    }
 }

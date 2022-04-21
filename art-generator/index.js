c4d56d1fe8a61791635c06857662edcb241d5390
aminbenmansour <amin.benmansour.10@gmail.com>
2022-04-21 18:00:28 +0100

    generate metadata

--- art-generator/index.js
+++ art-generator/index.js
@@ -32,10 +32,10 @@ function randElement(arr) {
 function getRandomName() {
     const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
     const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
+
     const randAdj = randElement(adjectives);
     const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
+    const name = `${randAdj}-${randName}`;
 
 
     if (takenNames[name] || !name) {
@@ -46,7 +46,7 @@ function getRandomName() {
     }
 }
 
-function getLayer(name, skip=0.0) {
+function getLayer(name, skip = 0.0) {
     const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
     const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
     const layer = svg.match(re)[0];
@@ -68,7 +68,7 @@ function createImage(idx) {
     const bg = randInt(5);
     const hair = randInt(7);
     const eyes = randInt(9);
-    const nose = randInt(4); 
+    const nose = randInt(4);
     const mouth = randInt(5);
     const beard = randInt(3);
     // 18,900 combinations
@@ -90,5 +90,21 @@ function createImage(idx) {
             .replace('<!-- nose -->', getLayer(`nose${nose}`))
             .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
             .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
+
+        const meta = {
+            name,
+            description: `A drawing of ${name.split('-').join(' ')}`,
+            image: `${idx}.png`,
+            attributes: [
+                {
+                    beard: '',
+                    rarity: 0.5
+                }
+            ]
+        }
+        writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
+        writeFileSync(`./out/${idx}.svg`, final)
+        svgToPng(idx)
     }
+
 }

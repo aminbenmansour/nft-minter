3ddd837c8f165d33dfc38cdef240aef4d457f8a5
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-03 15:48:33 +0100

    generate a failing test

--- marketplace-dapp/test/sample-test.js
+++ marketplace-dapp/test/sample-test.js
@@ -8,6 +8,13 @@ describe("UTG", function() {
 
     await nerdGuy.deployed();
 
+    const recipient = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+    const metadataURI = 'cid/test.png';
+
+    let balance = await nerdGuy.balanceOf(recipient);
+    expect(balance).to.equal(0);
+
+    const newlyMintedToken = await nerdGuy.payToMint(recipient, metadataURI, {value: ethers.utils.parseEther('0.01') });
     
   });
 });

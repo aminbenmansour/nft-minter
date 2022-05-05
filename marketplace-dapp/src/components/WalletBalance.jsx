c724629474773440f504db21144ed8943a452225
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-06 00:11:41 +0100

    getting the balance of the account

--- marketplace-dapp/src/components/WalletBalance.jsx
+++ marketplace-dapp/src/components/WalletBalance.jsx
@@ -8,6 +8,8 @@ const WalletBalance = () => {
     const getBalance = async () => {
         const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const balance = await provider.getBalance(account);
     };
 };
  

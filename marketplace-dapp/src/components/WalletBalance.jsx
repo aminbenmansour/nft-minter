edc2183df8dd1e3101bae4ff960a909e8e776904
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-06 00:03:26 +0100

    component to deal with wallet

---
+++ marketplace-dapp/src/components/WalletBalance.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const WalletBalance = () => {
+    
+    const [balance, SetBalance] = useState(0);
+
+    const getBalance = async () => {
+        
+    };
+};
+ 
+export default WalletBalance;

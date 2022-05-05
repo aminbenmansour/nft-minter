d7784fc6b613ea3378bda730182780737de55b82
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-05 23:41:37 +0100

    component to install metamask

---
+++ marketplace-dapp/src/components/Install.jsx
@@ -0,0 +1,10 @@
+const Install = () => {
+    return (
+        <div>
+            <h3>Follow the link to install the extension</h3>
+            <a href="https://metamask.io/download/">MetaMask</a>
+        </div>
+    );
+}
+
+export default Install ;

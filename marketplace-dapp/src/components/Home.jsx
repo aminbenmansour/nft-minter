c70bee791ceab4075fd300d0bda558812fa40a99
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-06 23:03:23 +0100

    init rendering minted NFTs + ability to mint new NFTs

--- marketplace-dapp/src/components/Home.jsx
+++ marketplace-dapp/src/components/Home.jsx
@@ -1,26 +1,50 @@
-import WalletBalance from './WalletBalance';
-
+import { useState } from 'react';
 import { ethers } from 'ethers';
 
+import WalletBalance from './WalletBalance';
 import NerdGuy from '../artifacts/contracts/UTG.sol/UnsatiableGuy.json';
+
+
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// get the end user (execute transactions)
+const signer = provider.getSigner();
+
+// get the smart contract
+const contract = new ethers.Contract(contractAddress, NerdGuy.abi, signer);
 
 const Home = () => {
 
-    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    const [totalMinted, setTotalMinted] = useState(0);
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    // get the end user (execute transactions)
-    const signer = provider.getSigner();
-
-    // get the smart contract
-    const contract = new ethers.Contract(contractAddress, NerdGuy.abi, signer);
+    const count = await contract.count();
+    setTotalMinted(parseInt(count))
 
     return (
         <div>
-            <WalletBalance/>
+            <WalletBalance />
+
+            <h1>Fired Guys NFT Collection</h1>
+            <div className="container">
+                <div className="row">
+                    {
+                        Array(totalMinted + 1).fill(0).map((_, i) => (
+                            <div>
+                                <NFTImage tokenId={i} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
         </div>
     );
+}
+
+function NFTImage({ tokenId, getCount }) {
+    
 }
 
 export default Home;

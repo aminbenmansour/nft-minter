de6b5e9de1b8765e81a097bb6494df91b41246b4
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-06 21:34:50 +0100

    displaying wallet balance in home

--- marketplace-dapp/src/components/Home.jsx
+++ marketplace-dapp/src/components/Home.jsx
@@ -1,7 +1,16 @@
+import NerdGuy from '../artifacts/contracts/UTG.sol/UnsatiableGuy.json';
+import { ethers } from 'ethers';
+
 const Home = () => {
+
+    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+
     return (
         <div>
-            
+            <WalletBalance/>
         </div>
     );
 }

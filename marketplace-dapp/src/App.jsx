5aab634b26ea363bd41a2fd2d53288d00df05332
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-05-18 13:34:16 +0100

    providing light and dark themes (default system)

--- marketplace-dapp/src/App.jsx
+++ marketplace-dapp/src/App.jsx
@@ -2,15 +2,48 @@ import Home from "./components/Home";
 import Install from "./components/Install";
 
 
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
+
+const lightTheme = createTheme({
+  type: 'light',
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
+
 
 const App = () => {
+
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
   if (window.ethereum) {
 
     return (
-      <NextUIProvider>
-        <Home />
-      </NextUIProvider>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className
+        }}
+      >
+        <NextUIProvider>
+          <div>
+            The current theme is: {type}
+            <Switch
+              checked={isDark}
+              onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+            />
+          </div>
+          <Home />
+        </NextUIProvider>
+      </NextThemesProvider >
     );
   } else {
     return (
